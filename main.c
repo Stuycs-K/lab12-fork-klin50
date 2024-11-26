297081fd03fa0dfcfdd00883db08898b79f7cb51
Kevin Lin <klin50@stuy.edu>
2024-11-26 15:25:19 -0500

    12

--- main.c
+++ main.c
@@ -2,19 +2,28 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <time.h>
 
 int main(){
   pid_t p;
+  int status;
   int PID = getpid();
   printf("%d about to create 2 child processes\n",PID);
-  int r = srand(time(NULL));
   p = fork();
   if(p != 0){
     p = fork();
   }
   if(p == 0){
     int PID = getpid();
-    printf("%d\n",PID);
+    srand(PID);
+    int r = rand()%5 + 1;
+    printf("%d %dsec\n",PID,r);
+    sleep(r);
+    printf("%d finished after %dsec\n",PID,r);
+    return(r);
+  }
+  if(p != 0){
+    pid_t d = wait(&status);
+    int r = WEXITSTATUS(status);
+    printf("Main Process %d is done. Child %d slept for %dsec\n",PID,d,r);
   }
 }
